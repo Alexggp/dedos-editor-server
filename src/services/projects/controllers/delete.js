72da83d4da1ceb14d26c455927eef4f9895c69a3
inditex <alejandroggp@ext.inditex.com>
2022-06-22 20:35:16 +0200

    deleting projects and its inside elemetns

--- src/services/projects/controllers/delete.js
+++ src/services/projects/controllers/delete.js
@@ -1,14 +1,22 @@
-const { ProjectsModel } = require('../../../database/models');
+const {ProjectsModel, ActivitiesModel, AreasModel, TokensModel} = require('../../../database/models.js');
 
 
 const controller = async (req, res)=>{
   console.log('/projects - DELETE');
   try{
-    const project = await ProjectsModel.deleteOne({ _id: req.params.id });
+    const projectId =  req.params.id 
+    const filter = { projectId: projectId };
+
+    const project = await ProjectsModel.deleteOne({ _id: projectId });
     if (!project.deletedCount) {
       console.log('/projects - Not Found');
       return res.status(404).send();
     }
+
+    await ActivitiesModel.deleteMany(filter);
+    await AreasModel.deleteMany(filter);
+    await TokensModel.deleteMany(filter);
+
     console.log('/projects - Deleted');
     res.send()
   }
